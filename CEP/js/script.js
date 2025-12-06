960a6614ea231088220f800f710a8204b2753657
Gabriel Neto <140651218+ChoccyMilk8641@users.noreply.github.com>
2025-12-06 14:46:24 +0000

    att

--- CEP/js/script.js
+++ CEP/js/script.js
@@ -1,13 +1,40 @@
-const inputCep = document.getElementById('cep');
-const buscarBtn = document.getElementById('buscarBtn');
-const loading = document.getElementById('loading');
-const erro = document.getElementById('erro');
-const resultado = document.getElementById('resultado');
-const mensagemErro = document.getElementById('mensagemErro');
+ 
+        const input = document.querySelector('#cep')
+        const button = document.getElementById("buscarBtn")
+        const cepDiv = document.getElementById("cepResultado")
+        const logradouroDiv = document.getElementById("logradouro")
+        const bairroDiv = document.getElementById("bairro")
+        const estadoDiv = document.getElementById("estado")
+        const cidadeDiv = document.getElementById("cidade")
+        const resultado = document.getElementById("resultado")
+        
+        button.addEventListener('click', async (e)=>{
+            const inputValue = input.value
+            const data = await fetchData(inputValue)
 
-function formatarCep (valor) {
-    valor = valor.replace(/\D/g, '');
-    if (valor.length > 5) {
-        valor = valor.replace(/\^)
-    }
-}
+            cepDiv.textContent = data.cep
+            logradouroDiv.textContent = data.logradouro
+            bairroDiv.textContent = data.bairro
+            estadoDiv.textContent = data.estado
+            cidadeDiv.textContent = data.localidade
+
+            resultado.classList.remove("hidden")
+            
+        })
+
+        input.addEventListener('input', (e)=>{
+            const cep = e.target.value
+
+            if(cep.length > 8){
+                button.removeAttribute("disabled")
+            } else {
+                button.setAttribute("disabled", "")
+            }
+        })
+
+        async function fetchData(cep){
+            const data = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+            const jsonData = await data.json()
+
+            return jsonData
+        }
